b6bc474008f7c944c8ab7baa3cb8827492cc3537
Johaned <johan.tique@codescrum.com>
2014-11-06 23:30:07 -0500

    add protected ping route

--- app/api/songbook/modules/ping.rb
+++ app/api/songbook/modules/ping.rb
@@ -2,11 +2,20 @@ module Songbook
   module Modules
     class Ping < Grape::API
       format :json
+
       version :v1 do
+
         desc 'Returns pong.'
         get :ping do
           { ping: params[:pong] || 'pong' }
         end
+
+        desc 'returns pong if authentication process is valid.'
+        get :protected_ping do
+          error!('401 Unauthorized', 401) unless params[:songbook_token] == 'the_most_secure_token'
+          { ping: params[:pong] || 'pong' }
+        end
+
       end
     end
   end
