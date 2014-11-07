9136a54191e4dc289e7a2ea0268f8b6102397af6
Johaned <johan.tique@codescrum.com>
2014-11-06 22:21:45 -0500

    add ping test with a param

--- spec/api/ping_spec.rb
+++ spec/api/ping_spec.rb
@@ -3,4 +3,9 @@ describe 'Ping', :type => :request do
     get '/api/ping'
     expect(response.body).to eq({ ping: 'pong' }.to_json)
   end
+
+  it 'ping with a parameter' do
+    get '/api/ping?pong=test'
+    expect(response.body).to eq({ ping: 'test' }.to_json)
+  end
 end
