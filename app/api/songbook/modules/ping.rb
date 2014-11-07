a40f502186686bbb2f19a254d26885e1fbc24648
Johaned <johan.tique@codescrum.com>
2014-11-06 23:31:40 -0500

    refactor auth logic into local helper

--- app/api/songbook/modules/ping.rb
+++ app/api/songbook/modules/ping.rb
@@ -3,6 +3,12 @@ module Songbook
     class Ping < Grape::API
       format :json
 
+      helpers do
+        def authenticated_user?
+          error!('401 Unauthorized', 401) unless params[:songbook_token] == 'the_most_secure_token'
+        end
+      end
+
       version :v1 do
 
         desc 'Returns pong.'
@@ -12,7 +18,7 @@ module Songbook
 
         desc 'returns pong if authentication process is valid.'
         get :protected_ping do
-          error!('401 Unauthorized', 401) unless params[:songbook_token] == 'the_most_secure_token'
+          authenticated_user?
           { ping: params[:pong] || 'pong' }
         end
 
