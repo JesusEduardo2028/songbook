bff595f96d113910f34bcf8e2e27aa33501ea42e
Johaned <johan.tique@codescrum.com>
2014-11-06 22:23:57 -0500

    [testing] add api version in ping path

--- spec/api/ping_spec.rb
+++ spec/api/ping_spec.rb
@@ -1,11 +1,11 @@
 describe 'Ping', :type => :request do
   it 'ping' do
-    get '/api/ping'
+    get '/api/v1/ping'
     expect(response.body).to eq({ ping: 'pong' }.to_json)
   end
 
   it 'ping with a parameter' do
-    get '/api/ping?pong=test'
+    get '/api/v1/ping?pong=test'
     expect(response.body).to eq({ ping: 'test' }.to_json)
   end
 end
