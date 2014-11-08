725e878f2959066fa92874f93e58c0efa0c3cbac
Johaned <johan.tique@codescrum.com>
2014-11-08 00:50:08 -0500

    add find_access class method and start_expiry_period instance method to AccessGrant class

--- app/models/access_grant.rb
+++ app/models/access_grant.rb
@@ -3,11 +3,29 @@ class AccessGrant
   include Mongoid::Timestamps
 
   field :access_token, :type => String
+  field :access_token_expires_at, :type => Time
 
   belongs_to :user
 
   before_create :gen_tokens
+  after_create :start_expiry_period!
 
+  # defines a class mehtod that allows find any matches regarding specific token
+  # @param access_token [String], secured key associated with temporal token
+  # @return [AccesGrant], access_grant instance with reference to current user
+  def self.find_access(access_token)
+    where(:access_token => access_token).
+      any_of(
+             {:access_token_expires_at => nil},
+             {:access_token_expires_at.gt => Time.now}).first
+  end
+
+  # Update access_token_expires_at field
+  def start_expiry_period!
+    self.update_attribute(:access_token_expires_at, 2.hours.from_now) unless self.access_token_expires_at.present?
+  end
+
+  protected
   # generates tokens associated with code, access_token and refresh_token
   def gen_tokens
     self.access_token = SecureRandom.hex(16)
