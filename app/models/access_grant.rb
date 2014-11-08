c30951b6b7f2dfd79709b276a159fca31baddeef
Johaned <johan.tique@codescrum.com>
2014-11-08 00:35:12 -0500

    make <verifies if access token have been created> spec be green

--- app/models/access_grant.rb
+++ app/models/access_grant.rb
@@ -1,3 +1,16 @@
 class AccessGrant
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  field :access_token, :type => String
+
+  belongs_to :user
+
+  before_create :gen_tokens
+
+  # generates tokens associated with code, access_token and refresh_token
+  def gen_tokens
+    self.access_token = SecureRandom.hex(16)
+  end
 
 end
