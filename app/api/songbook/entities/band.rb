f61e6907e4d910623ac3d0ecf2e9de7ff37848ef
Johaned <johan.tique@codescrum.com>
2014-11-08 03:49:25 -0500

    manipulate entity in order to transform the id presentation

--- app/api/songbook/entities/band.rb
+++ app/api/songbook/entities/band.rb
@@ -1,7 +1,9 @@
 module Songbook
   module Entities
     class Band < Grape::Entity
-      expose :id, documentation: { type: "string", desc: "id of the entry", example: '172a66834fb7802c28000003' }
+      expose :id, documentation: { type: "string", desc: "id of the entry", example: '172a66834fb7802c28000003' } do |band, options|
+        band.id.to_s
+      end
       expose :name, documentation: { type: "string", desc: "band name", example: "Superlitio" }
       expose :biography, documentation: { type: "string", desc: "band biography", example: "short bio" }
       expose :genre, documentation: { type: "string", desc: "band genre", example: "Rock" }
