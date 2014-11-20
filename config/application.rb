c1f5540fd37d26ce570fcfc465317f1296431dcd
Johan Tique <johan.tique@codescrum.com>
2014-11-20 11:45:02 -0500

    add mongoid configfile hack

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,14 @@ module Songbook
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    ###### Heroku deployment hack ######
+    # copy mongoid config example file into 'real' file
+    if Rails.env.production?
+      config_root = File.join(Rails.root, 'config')
+      mongoid_example_path = File.join(config_root, 'mongoid.yml.example')
+      mongoid_real_path = File.join(config_root, 'mongoid.yml')
+      `cp #{mongoid_example_path} #{mongoid_real_path}`
+    end
   end
 end
