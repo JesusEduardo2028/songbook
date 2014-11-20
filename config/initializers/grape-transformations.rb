808c23c5cffc3979f1caf142b07702a7d1d5407c
Johan Tique <johan.tique@codescrum.com>
2014-11-20 10:45:59 -0500

    execute rails g grape:transformations:install

---
+++ config/initializers/grape-transformations.rb
@@ -0,0 +1,4 @@
+# it loads automatically entities that are associated with models in your app
+Grape::Transformations.setup do |config|
+  config.load_entities_from File.join('songbook', 'entities')
+end
